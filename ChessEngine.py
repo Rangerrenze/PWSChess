25661b6b4ad9453b297e6ed02703f1a1f4846e64
Rangerrenze <51740186+Rangerrenze@users.noreply.github.com>
2021-03-16 16:43:13 +0100

    Update ChessEngine.py

--- ChessEngine.py
+++ ChessEngine.py
@@ -4,6 +4,8 @@ import ChessMinimax
 import pygame as p
 import numpy as np
 import math
+import timeit
+import random
 
 class GameState ():
     def __init__(self):
@@ -16,6 +18,8 @@ class GameState ():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]])
+
+
         self.WhiteToMove = True
         self.turn = 1
         self.moveLog = []
@@ -30,25 +34,108 @@ class GameState ():
                                             self.currentCastlingRights.bks, self.currentCastlingRights.bqs)]
         self.castleRule = ChessMain.castleRule
         self.empassentPossible = False
+        self.minimaxGameOver = False
+        self.OGTurnRight = True
+        self.whitePawnPositionValue = [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [50, 50, 50, 50, 50, 50, 50, 50],
+            [10, 10, 20, 30, 30, 20, 10, 10],
+            [5, 5, 10, 25, 25, 10, 5, 5],
+            [1, 1, 1, 20, 20, 1, 1, 1],
+            [5, -5, -10, 50, 50, -10, -5, 5],
+            [5, 10, 10, -20, -20, 10, 10, 5],
+            [1, 1, 1, 1, 1, 1, 1, 1]]
+
+        self.blackPawnPositionValue = [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [5, 10, 10, -20, -20, 10, 10, 5],
+            [5, -5, -10, 50, 50, -10, -5, 5],
+            [1, 1, 1, 20, 20, 1, 1, 1],
+            [5, 5, 10, 25, 25, 10, 5, 5],
+            [10, 10, 20, 30, 30, 20, 10, 10],
+            [50, 50, 50, 50, 50, 50, 50, 50],
+            [1, 1, 1, 1, 1, 1, 1, 1]]
 
     def getBoard(self):
         return self.board
 
     def aiMove(self):
         if not self.WhiteToMove:
-            move = ChessMinimax.AI().minimax(self.board, 5, 5, True, self.WhiteToMove, math.inf, -math.inf)[0]
+            start = timeit.timeit()
+            move = self.minimax(5, 5, True, math.inf, -math.inf)[0]
+            end = timeit.timeit()
+            time = end-start
+            print("Minimax time = ", time)
+
             if move == "None moves" or move == None:
                 print("None moves error")
                 return None
             else:
                 return move
         if self.WhiteToMove:
-            move = ChessMinimax.AI().minimax(self.board, 5, 5, True, self.WhiteToMove, math.inf, -math.inf)[0]
+            start = timeit.timeit()
+            move = self.minimax(5, 5, True, math.inf, -math.inf)[0]
+            end = timeit.timeit()
+            time = end-start
+            print("Minimax time = ", time)
             if move == "None moves" or move == None:
                 print("None moves error")
                 return None
             else:
                 return move
+
+    def minimax(self, depth, OGDepth, maxPlayer, alpha, beta):
+        if depth == OGDepth:
+            self.OGTurnRight = self.WhiteToMove
+        moveGen = ChessMoveGen.MoveGeneration().getMoves(self.board, self.WhiteToMove, True, self.castlingCopy)
+        moves = moveGen[0]
+        self.minimaxGameOver = moveGen[1]
+        if depth == 0 or self.minimaxGameOver:
+            return None, ChessMinimax.AI().evaluateBoard(self.board, self.OGTurnRight)
+        if len(moves) == 0:
+            return None, ChessMinimax.AI().evaluateBoard(self.board, self.OGTurnRight)
+        if moves == None:
+            return None, ChessMinimax.AI().evaluateBoard(self.board, self.OGTurnRight)
+        ri = random.randint(0, len(moves)-1)
+        bestMove = moves[ri]
+        if maxPlayer:
+            maxEval = -math.inf
+            for move in moves:
+                if move == "None Moves" or move == None:
+                    return bestMove, maxEval
+                self.makeMove(move)
+                self.updateCastlingRights(move)
+                currentEval = self.minimax(depth -1, OGDepth, False, alpha, beta)[1]
+                self.undoMove()
+
+                if currentEval > maxEval:
+                    maxEval = currentEval
+                    bestMove = move
+                alpha = max(alpha, currentEval)
+                if alpha <= beta:
+                    break
+            return bestMove, maxEval
+
+        else:
+            minEval = math.inf
+            for move in moves:
+                if move == "None Moves" or move == None:
+                    return move, minEval
+                self.makeMove(move)
+                self.updateCastlingRights(move)
+                currentEval = self.minimax(depth - 1, OGDepth, True, alpha, beta)[1]
+                self.undoMove()
+                if currentEval < minEval:
+                    minEval = currentEval
+                    bestMove = move
+                beta = min(beta, currentEval)
+                if beta <= alpha:
+                    break
+            return bestMove, minEval
+
+
+
+
 
     def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
@@ -121,7 +208,11 @@ class GameState ():
 
     def getValidMoves(self):
         if not self.gameOver:
+            start = timeit.timeit()
             movegen = ChessMoveGen.MoveGeneration().getMoves(self.board, self.WhiteToMove, False, self.castlingCopy)
+            end = timeit.timeit()
+            time = end-start
+            print("Time test", time)
             moves = movegen[0]
             return moves
 
